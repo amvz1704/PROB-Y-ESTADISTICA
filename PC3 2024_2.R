f213514062f5afe7b2b85e3723c8205f88725c05
amvz1704 <amvz17042013@gmail.com>
2024-10-21 22:12:34 -0500

    ADD PC3

---
+++ PC3 2024_2.R
@@ -0,0 +1,33 @@
+valA = runif(5000)
+valB = runif(5000)
+valC = runif(5000)
+
+error1 = rnorm(5000, 0, 1)
+error2 = rnorm(5000, 0, 1)
+error3 = rnorm(5000, 0, 1)
+
+xpA = 10/((-log(valA))^(1/10))
+xpB = 30*valB
+xpC = -10*log(1-valC)
+
+y1 = max(12, xpA) + error1
+y2 = pmax(5, xpB + error2)
+y3 = min(10, xpC + 5) + error3
+
+fy1 = density(y1)
+fy2 = density(y2)
+fy3 = density(y3)
+
+plot(fy1, col = 1, lwd = 2,
+     main = "", xlab = "x", ylab = "Y1")
+EY1 = sum(fy1$x * fy1$y) * diff(fy1$x[1:2])
+
+plot(fy2, col = 1, lwd = 2,
+     main = "", xlab = "x", ylab = "Y2")
+EY2 = sum(fy2$x * fy2$y) * diff(fy2$x[1:2])
+
+plot(fy3, col = 1, lwd = 2,
+     main = "", xlab = "x", ylab = "Y3")
+EY3 = sum(fy3$x * fy3$y) * diff(fy3$x[1:2])
+
+
