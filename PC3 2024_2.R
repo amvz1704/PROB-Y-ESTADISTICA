cce4b332b78f8f3b011c83d55583ae098c8d9d80
amvz1704 <amvz17042013@gmail.com>
2024-10-21 22:38:40 -0500

    Pregunta 2 y 3

--- PC3 2024_2.R
+++ PC3 2024_2.R
@@ -10,9 +10,9 @@ xpA = 10/((-log(valA))^(1/10))
 xpB = 30*valB
 xpC = -10*log(1-valC)
 
-y1 = max(12, xpA) + error1
+y1 = pmax(12, xpA) + error1
 y2 = pmax(5, xpB + error2)
-y3 = min(10, xpC + 5) + error3
+y3 = pmin(10, xpC + 5) + error3
 
 fy1 = density(y1)
 fy2 = density(y2)
@@ -29,5 +29,34 @@ EY2 = sum(fy2$x * fy2$y) * diff(fy2$x[1:2])
 plot(fy3, col = 1, lwd = 2,
      main = "", xlab = "x", ylab = "Y3")
 EY3 = sum(fy3$x * fy3$y) * diff(fy3$x[1:2])
+
+
+#cuartiles Q1
+
+quantile(y1, 0.25)
+quantile(y1, 0.5)
+quantile(y1, 0.75)
+
+#cuartiles Q2
+quantile(y2, 0.25)
+quantile(y2, 0.5)
+quantile(y2, 0.75)
+
+#cuartiles Q3
+quantile(y3, 0.25)
+quantile(y3, 0.5)
+quantile(y3, 0.75)
+
+
+mayorY = pmax(y1, y2, y3)
+mean(mayorY)
+sd(mayorY)
+#cuartiles Y
+quantile(mayorY, 0.25)
+quantile(mayorY, 0.5)
+quantile(mayorY, 0.75)
+quantile(mayorY, 1)
+plot(density(mayorY), col = 1, lwd = 2,
+     main = "", xlab = "x", ylab = "Y")
 
 
